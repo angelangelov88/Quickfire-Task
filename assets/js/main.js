9789a72b992cfc1d42211ba36a0a441aa77369cf
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-02-13 19:49:22 +0000

    Responsiveness
    I added some responsiveness to the page.
    I made sure that everything is visible and looks as intended.
    I had a lot of issues with the image slider as it is far too difficult to style it.
    I added a hamburger menu on small screens and added a function to open/close it. Also added some conditions to make sure it gets closed if the user resizes the page.
    Made some changes to the product details part to make it flex-direction: column and that way it will appear under the image slider.
    Also added some other styles to make sure the rest of the page is responsive.
    Had some issues with the footer too. It has 4 different divs with all the links. I decided to decrease the font-size for iPad kind of screen and then changed the flex to column in order to show each one on a separate line.
    Also added some validation and JavaScript to the newsletter form in order to make it submit. I added an alert on submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,52 @@
+//Hamburger 
+
+// This is the code for the hamburger menu on small screens. The dropdown appears on click on the hamburger (invoking the OpenCloseNav function). Once shown I add the blocker div to complete the page and allow the user to click anywhere but the dropdown (including the X) to close it. 
+
+const headerSmall = document.getElementById('header-small');
+const menuLine = document.getElementsByClassName('menu-lines')[0];
+const menuX = document.getElementsByClassName('menu-x')[0];
+const menuTitle = document.getElementsByClassName('menu-title')[0];
+const blocker = document.getElementsByClassName('blocker')[0];
+const body = document.body;
+
+function openCloseNav() {
+  if (headerSmall.style.display === "block") {
+    headerSmall.style.display = "none";
+    menuLine.style.display = "block";
+    menuX.style.display = 'none';
+    menuTitle.style.display = 'block';
+    blocker.style.display = 'none'
+    body.style.overflow = 'auto';
+} else {
+    headerSmall.style.display = "block";
+    menuLine.style.display = "none";
+    menuX.style.display = 'block';
+    menuTitle.style.display = 'none';
+    blocker.style.display = 'block'
+    body.style.overflow = 'hidden';
+  }
+}
+
+//Function to make the small screen navbar disappear if the user changes to bigger screen even if it has been opened on the small one (for example if the user changes between portrait and landscape modes). It also checks if the hamburger has the lines or X and makes sure it is correctly displayed after resizing of the screen
+
+var x = window.matchMedia("(max-width: 991px)")
+removeNav(x)
+x.addListener(removeNav)
+
+function removeNav(x) {
+  if (x.matches) {
+    headerSmall.style.display = "none";
+    menuLine.style.display = "block";
+    menuX.style.display = 'none';
+    menuTitle.style.display = 'block';
+} else {
+    headerSmall.style.display = "none";
+    blocker.style.display = "none";
+    body.style.overflow = 'auto';
+}
+}
+
+
 //Countdown timer
 var date = new Date();
 var second = date.getSeconds();
@@ -47,14 +96,19 @@ const sizeModal = document.getElementById('size-modal')
 function openCloseModal() {
   if (sizeModal.style.display == 'block') {
     sizeModal.style.display = 'none'
+    event.preventDefault()
+
   } else {
     sizeModal.style.display = 'block'
+    event.preventDefault()
+
   }
 }
 
 
 
 
+//Magnifying glass
 
 $(document).ready(function() {
   $('.magnified-image').magnify();
@@ -65,4 +119,12 @@ $(document).ready(function() {
     speed: 400
     });
 });
+
+
+//form
+
+function alertFunction() {
+  document.getElementById('input-email').value = ''
+  alert('Congrats! You have signed up for our newsletter')
+}
 
